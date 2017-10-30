7976798efc455d896fb8d6823989a796a9385581
baixue <baixuexue123@hotmail.com>
2017-10-30 22:26:26 +0800

    comments

--- comments/models.py
+++ comments/models.py
@@ -4,6 +4,10 @@ from blog.models import Post
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post)
-    author = models.CharField('author')
-    content = models.TextField('content')
+    author = models.CharField(max_length=48)
+    text = models.TextField()
+    approved = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    ip_address = models.GenericIPAddressField(blank=True, null=True)
+
+    post = models.ForeignKey(Post, related_name='comments')
