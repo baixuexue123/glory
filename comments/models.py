b0c0df79b783acc2f29c51b73e940fee06e686b1
bxf_100_u <baixuexue123@hotmail.com>
2017-10-31 18:57:34 +0800

    comments ing

--- comments/models.py
+++ comments/models.py
@@ -1,13 +1,28 @@
-from django.db import models
+from django.db import models, transaction
 
 from blog.models import Post
 
 
 class Comment(models.Model):
     author = models.CharField(max_length=48)
-    text = models.TextField()
-    approved = models.BooleanField(default=True)
+    text = models.TextField(max_length=2048)
+    active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
-    ip_address = models.GenericIPAddressField(blank=True, null=True)
+    ip_address = models.CharField(max_length=24, default='')
+
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='children')
+    tree_path = models.CharField(max_length=255, editable=False)
 
     post = models.ForeignKey(Post, related_name='comments')
+
+    @property
+    def depth(self):
+        return len(self.tree_path.split('/'))
+
+    @property
+    def root_id(self):
+        return int(self.tree_path.split('/')[0])
+
+    @property
+    def root_path(self):
+        return Comment.objects.filter(pk__in=self.tree_path.split('/')[:-1])
